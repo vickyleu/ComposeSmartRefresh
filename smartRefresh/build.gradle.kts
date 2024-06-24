c8a0e679a97fca825a98d3708ba85ccd4b106ec1
vicky leu <yu6564172@gmail.com>
2024-06-24 19:34:14 +0800

    remove unnecessary folder

--- smartRefresh/build.gradle.kts
+++ smartRefresh/build.gradle.kts
@@ -1,8 +1,14 @@
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.dokka)
+    id("maven-publish")
 }
 
 
@@ -76,4 +82,169 @@ android {
     kotlin {
         jvmToolchain(17)
     }
+}
+
+
+
+
+buildscript {
+    dependencies {
+        val dokkaVersion = libs.versions.dokka.get()
+        classpath("org.jetbrains.dokka:dokka-base:$dokkaVersion")
+    }
+}
+
+//group = "io.github.ltttttttttttt"
+////上传到mavenCentral命令: ./gradlew publishAllPublicationsToSonatypeRepository
+////mavenCentral后台: https://s01.oss.sonatype.org/#stagingRepositories
+//version = "${libs.versions.compose.plugin.get()}.beta1"
+
+group = "com.vickyleu.refresh"
+version = "1.0.2"
+
+
+tasks.withType<PublishToMavenRepository> {
+    val isMac = DefaultNativePlatform.getCurrentOperatingSystem().isMacOsX
+    onlyIf {
+        isMac.also {
+            if (!isMac) logger.error(
+                """
+                    Publishing the library requires macOS to be able to generate iOS artifacts.
+                    Run the task on a mac or use the project GitHub workflows for publication and release.
+                """
+            )
+        }
+    }
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.dokkaHtml)
+    from(tasks.dokkaHtml.flatMap(DokkaTask::outputDirectory))
+    archiveClassifier = "javadoc"
+}
+
+
+tasks.dokkaHtml {
+    // outputDirectory = layout.buildDirectory.get().resolve("dokka")
+    offlineMode = false
+    moduleName = "compose"
+
+    // See the buildscript block above and also
+    // https://github.com/Kotlin/dokka/issues/2406
+//    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+////        customAssets = listOf(file("../asset/logo-icon.svg"))
+////        customStyleSheets = listOf(file("../asset/logo-styles.css"))
+//        separateInheritedMembers = true
+//    }
+
+    dokkaSourceSets {
+        configureEach {
+            reportUndocumented = true
+            noAndroidSdkLink = false
+            noStdlibLink = false
+            noJdkLink = false
+            jdkVersion = 17
+            // sourceLink {
+            //     // Unix based directory relative path to the root of the project (where you execute gradle respectively).
+            //     // localDirectory.set(file("src/main/kotlin"))
+            //     // URL showing where the source code can be accessed through the web browser
+            //     // remoteUrl = uri("https://github.com/mahozad/${project.name}/blob/main/${project.name}/src/main/kotlin").toURL()
+            //     // Suffix which is used to append the line number to the URL. Use #L for GitHub
+            //     remoteLineSuffix = "#L"
+            // }
+        }
+    }
+}
+
+val properties = Properties().apply {
+    runCatching { rootProject.file("local.properties") }
+        .getOrNull()
+        .takeIf { it?.exists() ?: false }
+        ?.reader()
+        ?.use(::load)
+}
+// For information about signing.* properties,
+// see comments on signing { ... } block below
+val environment: Map<String, String?> = System.getenv()
+extra["githubToken"] = properties["github.token"] as? String
+    ?: environment["GITHUB_TOKEN"] ?: ""
+
+publishing {
+    val gitRepoName = rootProject.name
+    val projectName = project.name
+    repositories {
+        /*maven {
+            name = "CustomLocal"
+            url = uri("file://${layout.buildDirectory.get()}/local-repository")
+        }
+        maven {
+            name = "MavenCentral"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = extra["ossrhUsername"]?.toString()
+                password = extra["ossrhPassword"]?.toString()
+            }
+        }*/
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/vickyleu/${gitRepoName}")
+            credentials {
+                username = "vickyleu"
+                password = extra["githubToken"]?.toString()
+            }
+        }
+    }
+
+    afterEvaluate {
+        publications.withType<MavenPublication> {
+            artifactId = artifactId.replace(project.name, projectName.lowercase())
+            artifact(javadocJar) // Required a workaround. See below
+            pom {
+                url = "https://github.com/vickyleu/${gitRepoName}"
+                name = projectName
+                description = """
+                Visit the project on GitHub to learn more.
+            """.trimIndent()
+                inceptionYear = "2024"
+                licenses {
+                    license {
+                        name = "Apache-2.0 License"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "loren-moon"
+                        name = "loren-moon"
+                        email = ""
+                        roles = listOf("Mobile Developer")
+                        timezone = "GMT+8"
+                    }
+                }
+                contributors {
+                    // contributor {}
+                }
+                scm {
+                    tag = "HEAD"
+                    url = "https://github.com/vickyleu/${gitRepoName}"
+                    connection = "scm:git:github.com/vickyleu/${gitRepoName}.git"
+                    developerConnection = "scm:git:ssh://github.com/vickyleu/${gitRepoName}.git"
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "https://github.com/vickyleu/${gitRepoName}/issues"
+                }
+                ciManagement {
+                    system = "GitHub Actions"
+                    url = "https://github.com/vickyleu/${gitRepoName}/actions"
+                }
+            }
+        }
+    }
+}
+
+// TODO: Remove after https://youtrack.jetbrains.com/issue/KT-46466 is fixed
+//  Thanks to KSoup repository for this code snippet
+tasks.withType(AbstractPublishToMaven::class).configureEach {
+    dependsOn(tasks.withType(Sign::class))
 }
